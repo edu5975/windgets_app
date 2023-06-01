6fd128f32cf996ed1f1b85808860a8870203f009
Your Name <you@example.com>
2023-06-01 17:22:06 -0600

    animated container terminado

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,11 +1,75 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animated_screen';
   const AnimatedScreen({super.key});
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+  Curve curve = Curves.easeOutCubic;
+
+  void changeShape() {
+    final random = Random();
+    width = random.nextInt(300) + 50;
+    height = random.nextInt(300) + 50;
+    borderRadius = random.nextDouble() * 100 + 10;
+    color = Color.fromRGBO(
+      random.nextInt(255),
+      random.nextInt(255),
+      random.nextInt(255),
+      1,
+    );
+    switch (random.nextInt(5)) {
+      case 0:
+        curve = Curves.bounceIn;
+        break;
+      case 1:
+        curve = Curves.slowMiddle;
+        break;
+      case 2:
+        curve = Curves.easeInCirc;
+        break;
+      case 3:
+        curve = Curves.easeInExpo;
+        break;
+      case 4:
+        curve = Curves.fastOutSlowIn;
+        break;
+    }
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Container'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          curve: curve,
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeShape,
+        child: const Icon(Icons.play_arrow_outlined),
+      ),
+    );
   }
 }
